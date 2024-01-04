f87956558bee827a0829ae32e3d322107255ac25
Mafalda <mafalda.ventura@live.com.pt>
2024-01-04 11:41:08 +0000

    details

--- routes/skills.js
+++ routes/skills.js
@@ -5,7 +5,7 @@ var skillsCtrl = require('../controllers/skills')
 // GET /skills
 router.get('/', skillsCtrl.index)
 
-// GET /skills
+// GET /skills/:id
 router.get('/:id', skillsCtrl.show)
 
 module.exports = router;
